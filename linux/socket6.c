c7f7370db812ae0c423e184326db51893205dba7
samiam95124 <samiam@moorecad.com>
2019-10-29 08:50:05 -0700

    Sundry changes
    === Files changed ===
    
    modified:   linux/network.not
    
    1. Update to-do list.
    
    modified:   linux/socket6.c
    
    1. This refactor was to research IPv6 issues. Generally this program is
    useless beyond that.
    
    === Bug numbers affected ===
    
    === Notes ===

--- linux/socket6.c
+++ linux/socket6.c
@@ -24,6 +24,38 @@ void error(const char* es)
 
 }
 
+
+void doconnect(struct sockaddr* saddr, int fn)
+
+{
+
+    char buff[1024];
+    int r;
+
+    /* connect to server */
+    r = connect(fn, saddr, sizeof(saddr));
+    if (r < 0) error("Connect Failed");
+
+    /* send request */
+    sprintf(buff, "GET %s HTTP/1.1\r\n", "/");
+    write(fn, buff, strlen(buff));
+
+    sprintf(buff, "Host: %s\r\n\r\n", "www.example.com");
+    write(fn, buff, strlen(buff));
+    do {
+
+        r = read(fn, buff, sizeof(buff));
+        if (r > 0) {
+
+            buff[r] = 0;
+            printf("%s", buff);
+
+        }
+
+    } while (r);
+
+}
+
 int main(int argc, char *argv[])
 {
     int sockfd = 0, n = 0;
@@ -32,11 +64,12 @@ int main(int argc, char *argv[])
     int r;
     struct sockaddr* sap;
     struct addrinfo hints, *p;
+    int i;
 
     if(argc != 3)
     {
 
-        printf("Usage: socket <server> <page>\n");
+        printf("Usage: socket6 <server> <page>\n");
         exit(1);
 
     }
@@ -49,14 +82,41 @@ int main(int argc, char *argv[])
 
     hints.ai_family = AF_INET6;
     hints.ai_socktype = SOCK_STREAM;
-    //hints.ai_protocol = IPPROTO_TCP;
 
-    r = getaddrinfo(argv[1], "80", &hints, &p);
+    r = getaddrinfo(argv[1], NULL/*"80"*/, &hints, &p);
     if (r != 0) error(gai_strerror(r));
 
-    memcpy(&serv_addr, (struct sockaddr_in6*)p->ai_addr, sizeof(struct sockaddr_in6));
+//    memcpy(&serv_addr, (struct sockaddr_in6*)p->ai_addr, sizeof(struct sockaddr_in6));
     freeaddrinfo(p);
 
+    serv_addr.sin6_family = AF_INET6;
+
+    serv_addr.sin6_port = htons(80);
+
+    printf("flow info: %x\n", serv_addr.sin6_flowinfo);
+    printf("scope id: %x\n", serv_addr.sin6_scope_id);
+
+    serv_addr.sin6_addr.__in6_u.__u6_addr16[0] = htons(0x2606);
+    serv_addr.sin6_addr.__in6_u.__u6_addr16[1] = htons(0x2800);
+    serv_addr.sin6_addr.__in6_u.__u6_addr16[2] = htons(0x220);
+    serv_addr.sin6_addr.__in6_u.__u6_addr16[3] = htons(0x1);
+    serv_addr.sin6_addr.__in6_u.__u6_addr16[4] = htons(0x248);
+    serv_addr.sin6_addr.__in6_u.__u6_addr16[5] = htons(0x1893);
+    serv_addr.sin6_addr.__in6_u.__u6_addr16[6] = htons(0x25c8);
+    serv_addr.sin6_addr.__in6_u.__u6_addr16[7] = htons(0x1946);
+
+    /* Address: 2606:2800:220:1:248:1893:25c8:1946 (www.example.com) */
+
+    printf("Address: ");
+    for (i = 0; i < 8; i++) {
+
+        printf("%x", ntohs(serv_addr.sin6_addr.__in6_u.__u6_addr16[i]));
+        if (i < 7) printf(":");
+
+    }
+    printf("\n");
+
+#if 1
     /* connect to server */
     r = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     if (r < 0) error("Connect Failed");
@@ -78,6 +138,9 @@ int main(int argc, char *argv[])
         }
 
     } while (r);
+#else
+    doconnect((struct sockaddr*)&serv_addr, sockfd);
+#endif
 
     return 0;
 
