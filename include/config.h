6ba83f65601a3ef2213e7c0a9987557a0a6b2edb
samiam95124 <samiam@moorecad.com>
2021-01-16 23:00:52 -0800

    Add config.h file; add mantst.c
     Changes to be committed:
    	new file:   include/config.h
    
    1. Add config.h, the header file for the configurator.
    
    	new file:   mantst.c
    
    2. Add window management test.

---
+++ include/config.h
@@ -0,0 +1,31 @@
+/**//***************************************************************************
+
+Parse config file
+
+Parses config files into a tree structured database.
+
+*******************************************************************************/
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include <localdefs.h>
+
+/* Tree structured value record */
+
+typedef struct pa_value {
+
+    struct pa_value* next;    /* next value in list */
+    struct pa_value* sublist; /* new begin/end block */
+    string name;           /* name of node */
+    string value;          /* value of this node */
+
+} pa_value, *pa_valptr;
+
+void pa_prttre(pa_valptr list); /* print tree structured config list */
+pa_valptr pa_schlst(string id, pa_valptr root); /* search config list */
+void pa_merge(pa_valptr* root, pa_valptr newroot); /* merge config trees */
+void pa_configfile(string fn, pa_valptr* root); /* parse config file */
+void pa_config(pa_valptr* root); /* parse config files */
+
+#endif /* __CONFIG_H__ */
