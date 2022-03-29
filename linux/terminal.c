4441adc7fa0c20301f0d58fbe6f0ce59874cad79
samiam95124 <samiam@moorecad.com>
2022-03-28 22:14:47 -0700

    Fixed mouse clicks on line edit mode.

--- linux/terminal.c
+++ linux/terminal.c
@@ -2064,10 +2064,14 @@ static void readline(void)
                 if (er.amoubn == 1) {
 
                     /* mouse click button 1 */
-                    if (sc->cury == nmpy && inpptr+1 >= nmpx)
+                    if (sc->cury == nmpy && inpptr+1 >= nmpx) {
+
                         /* mouse position is within buffer space, set
                            position */
                         icursor(sc, nmpx, sc->cury);
+                        inpptr = nmpx-1;
+
+                    }
 
                 }
                 break;
